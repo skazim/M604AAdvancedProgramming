4e973fbad59c0dd2020f8b1a96e4096a4af9c011
skazim <kazimmuhammad10@gmail.com>
2024-07-04 14:44:12 +0200

    updating test

--- BicycleRoutesAnalysis/test/functional/test_model.py
+++ BicycleRoutesAnalysis/test/functional/test_model.py
@@ -2,22 +2,6 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from app import modelAnalysis,findModelAccuracy,shortText
 
-# def test_modelAnalysis_with_valid_data():
-#     bicycleRoutefile = pd.read_csv('././data/sampledata.csv')
-#     result = modelAnalysis(bicycleRoutefile)
-#     assert isinstance(result, dict)
-
-#     expected_keys = ["LogisticRegression", "DecisionTreeClassifier", "SVM",
-#                       "KNeighborsClassifier", "GNB", "RandomForestClassifier",
-#                         "AdaBoostClassifier", "GradientBoostingClassifier"]
-#     assert set(result.keys()) == set(expected_keys)
-
-# def test_modelAnalysis_with_none_data():
-#     result = modelAnalysis(None)
-#     assert isinstance(result, tuple)
-#     assert len(result) == 2
-#     assert result[1] == 500
-#     assert result[0]['File not found, please contact admin'] == 'Error while extraction data file, either file does not exist or corrupted.'
 
 def test_findModelAccuracy():
     x = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [2, 3, 4, 5, 6]})
